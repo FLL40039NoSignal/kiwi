e28cf9b0afe4200ef937cb0f6f9701cbfee48647
From Team Computer <fllteam40039@gmail.com>
2025-02-20 21:49:23 -0500

    Edit works, not saved

--- main.py
+++ main.py
@@ -47,7 +47,15 @@ try:
 
   trackball = Trackball( i2c )
   trackball.set_rgbw(0, 0, 128, 0)
-  
+  WIDTH = 128
+  HEIGHT = 128
+  BORDER = 2
+  #oled3 = SH1107_I2C(WIDTH, HEIGHT, i2c, address=0x3C, rotate=0)
+  oled3 = SSD1327_I2C(WIDTH, HEIGHT, i2c, addr=0x3D)
+  oled3.fill(0)
+  textWri = Writer(oled3, courier20)
+  textWri.printstring(" Reading \n Sensors \n\n May Run \n  Pumps  ")
+  oled3.show()
   # if the clock isn't set...
   if not enviro.is_clock_set():
     enviro.logging.info("> clock not set, synchronise from ntp server")
@@ -72,18 +80,7 @@ try:
     else:
       # no destination so go to sleep
       enviro.halt("! low disk space")
-      
-      
-  WIDTH = 128
-  HEIGHT = 128
-  BORDER = 2
-  #oled3 = SH1107_I2C(WIDTH, HEIGHT, i2c, address=0x3C, rotate=0)
-  oled3 = SSD1327_I2C(WIDTH, HEIGHT, i2c, addr=0x3D)
-  oled3.fill(0)
-  textWri = Writer(oled3, courier20)
-  #oled3.show()
   
-
   # TODO this seems to be useful to keep around?
   filesystem_stats = os.statvfs(".")
   enviro.logging.debug(f"> {filesystem_stats[3]} blocks free out of {filesystem_stats[2]}")
@@ -104,13 +101,44 @@ try:
   was_clicked = False
   
   ignore = 5
-  big_change = 25
-  while True:
+  big_change = 15
+  big_increment = 5
+  small_increment = 1
+  edit_loop = 45
+  while edit_loop>0:
+    edit_loop-=1
     up, down, left, right, switch, state = trackball.read()
     print("r:    {:02d}\tu:    {:02d}\td:    {:02d}\tl:     {:02d}\tswi:{:03d}\tsta:{}".format(right, up, down, left, switch, state))
     oled3.fill(0)
+    Writer.set_textpos(oled3, 0 , 0)
     textWri.printstring(" LAST TGT \nA {:3}{:1}{:3}\n\nB {:3}{:1}{:3}\n\nC {:3}{:1}{:3}".format(reading.get("moisture_a"), mode_a, config.moisture_target_a,reading.get("moisture_b"), mode_b, config.moisture_target_b, reading.get("moisture_c"), mode_c, config.moisture_target_c))
     oled3.show()
+    if state and not was_clicked:
+        # clearing up/down so doesn't move/incrememt with click
+        up = 0
+        down = 0
+        if mode_a == select:
+            mode_a = edit
+            trackball.set_rgbw(255,0,0,0)
+        elif mode_b == select:
+            mode_b = edit
+            trackball.set_rgbw(255,0,0,0)
+        elif mode_c == select:
+            mode_c = edit
+            trackball.set_rgbw(255,0,0,0)      
+        elif mode_a == edit:
+            mode_a = select
+            trackball.set_rgbw(0,255,0,0)
+        elif mode_b == edit:
+            mode_b = select
+            trackball.set_rgbw(0,255,0,0)
+        elif mode_c == edit:
+            mode_c = select
+            trackball.set_rgbw(0,255,0,0)
+        was_clicked = True
+    elif not state and was_clicked:
+        was_clicked = False
+
     if up > ignore:
         if mode_a == select:
             mode_c = select
@@ -121,6 +149,28 @@ try:
         elif mode_c == select:
             mode_b = select
             mode_c = ' '
+        elif mode_a == edit:
+            if up > big_change:
+                config.moisture_target_a +=big_increment
+            else:
+                config.moisture_target_a +=small_increment
+            if config.moisture_target_a >100:
+                config.moisture_target_a = 100      
+        elif mode_b == edit:
+            if up > big_change:
+                config.moisture_target_b +=big_increment
+            else:
+                config.moisture_target_b +=small_increment
+            if config.moisture_target_b >100:
+                config.moisture_target_b = 100      
+        elif mode_c == edit:
+            if up > big_change:
+                config.moisture_target_c +=big_increment
+            else:
+                config.moisture_target_c +=small_increment
+            if config.moisture_target_c >100:
+                config.moisture_target_c = 100      
+            
     elif down > ignore:
         if mode_a == select:
             mode_b = select
@@ -131,13 +181,32 @@ try:
         elif mode_c == select:
             mode_a = select
             mode_c = ' '
-    if state and not was_clicked:
-        #Change to edit
-        was_clicked = True
-    elif not state and was_clicked:
-        #Change to select
-        was_clicked = False
-    sleep(0.200)
+            
+        elif mode_a == edit:
+            if up > big_change:
+                config.moisture_target_a -=big_increment
+            else:
+                config.moisture_target_a -=small_increment
+            if config.moisture_target_a <0:
+                config.moisture_target_a = 0
+        elif mode_b == edit:
+            if up > big_change:
+                config.moisture_target_b -=big_increment
+            else:
+                config.moisture_target_b -=small_increment
+
+            if config.moisture_target_b <0:
+                config.moisture_target_b = 0            
+        elif mode_c == edit:
+            if up > big_change:
+                config.moisture_target_c -=big_increment
+            else:
+                config.moisture_target_c -=small_increment
+
+            if config.moisture_target_c <0:
+                config.moisture_target_c = 0
+    #sleep(0.1)
+  trackball.set_rgbw(0,0,255,0)
 
   # here you can customise the sensor readings by adding extra information
   # or removing readings that you don't want, for example:
